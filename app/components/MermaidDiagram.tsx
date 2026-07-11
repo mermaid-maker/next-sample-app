d4fad33d1e6c5c1f31985d10c121684afc55668a
Ahmed <ahmed.akod@outlook.com>
2026-07-11 15:36:21 -0400

    feat: add mmd file to public folder

--- app/components/MermaidDiagram.tsx
+++ app/components/MermaidDiagram.tsx
@@ -5,11 +5,11 @@ import Image from "next/image"
 export async function MermaidDiagram({chart}: {chart: string}){
 
     if (process.env.NODE_ENV == "development"){
-        const chart_content = await readFile(`${process.cwd()}/public/mermaid_diagrams/${chart}.mmd`, "utf-8")
+        const chart_content = await readFile(`${process.cwd()}/public/${chart}.mmd`, "utf-8")
 
         return <ClientMermaid chart={chart_content}/>
     }
     else if (process.env.NODE_ENV == "production") {
-        return <Image src={`mermaid_diagrams/${chart}.svg`} alt="mermaid_diagram" />
+        return <Image src={`/${chart}.svg`} alt="mermaid_diagram" />
     }
 }

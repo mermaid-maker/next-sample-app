7bc5352fb835992977447afb35694d99cab6ce08
Ahmed <ahmed.akod@outlook.com>
2026-07-11 16:27:50 -0400

    feat: added a basic title

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { MermaidDiagram } from "./components/MermaidDiagram"
 
 export default function Home() {
   return <div>
+    <h1>Add more mermaid diagrams!</h1>
     <MermaidDiagram chart="diagram"/>
   </div>
 

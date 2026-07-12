cff98d87462e4eb30ff3295b00bc7b6551618c44
Ahmed <ahmed.akod@outlook.com>
2026-07-12 12:45:24 -0400

    fix: add basepath for dev/prod

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,8 @@ const nextConfig: NextConfig = {
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  }
+  },
+  basePath: process.env.BASE_PATH,
 };
 
 export default nextConfig;
